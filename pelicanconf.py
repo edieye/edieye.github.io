f4af5b8d657c6a3f4da0d052fd10594d707b0179
edieye <e.y.96@hotmail.com>
2020-03-04 19:46:00 -0800

    adding favicon.ico, updating static paths for robots and ads txt

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,8 +31,11 @@ TAG_FEED_ATOM = 'feeds/{slug}.tag.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 # STATIC PATHS
-STATIC_PATHS = ['images', 'extra/CNAME']
-EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+STATIC_PATHS = ['images', 'extra/CNAME', 'extra/robots.txt', 'extra/favicon.ico', 'extra/ads.txt']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},
+                       'extra/robots.txt': {'path': 'robots.txt'},
+                       'extra/favicon.ico': {'path': 'favicon.ico'},
+                       'extra/ads.txt': {'path': 'ads.txt'}}
 
 PAGE_URL = 'about/'
 
@@ -45,14 +48,6 @@ LINKS = (('about', '/pages/about.html'),
 )
 
 DEFAULT_PAGINATION = 10
-
-# robots.txt and ads.txt for google ad sense
-# STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/ads.txt']
-# EXTRA_PATH_METADATA = {
-#     'extra/robots.txt': {'path': 'robots.txt'},
-#     'extra/favicon.ico': {'path': 'favicon.ico'},
-#     'extra/ads.txt': {'path': 'ads.txt'}
-# }
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
